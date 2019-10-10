9b5f353d2c56fc13826c60d26967a04f84f5a7a2
Payal Joshi <pjoshi@pdx.edu>
2019-10-09 20:06:55 -0700

    implemented a function that computes fibonacci series, mainly to understand the concept of modules in python

---
+++ fibo.py
@@ -0,0 +1,23 @@
+#understanding the concepts of module
+
+# This is a global variable that stores the name of the module, if not specified, 
+# it is the same as the file name.
+#__name__ = 'fibonacci' 
+
+def fib(n):
+	a, b = 0, 1
+	count = 0
+	
+	while count < n:
+		print(a,  end = ' ')
+		next = a + b
+		a = b
+		b = next
+		count+=1
+
+
+if __name__ == "__main__":
+	import sys
+	fib(int(sys.argv[1]))
+
+		
